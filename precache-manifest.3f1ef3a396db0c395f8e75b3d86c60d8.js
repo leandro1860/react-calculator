4d276fda8807724d22817e170ee7999f6c9fcb99
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-07-21 21:25:21 -0300

    Updates

---
+++ precache-manifest.3f1ef3a396db0c395f8e75b3d86c60d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1daab02863b087f71d22a68b04e6ec34",
+    "url": "/leandro1860/react-calculator/index.html"
+  },
+  {
+    "revision": "d45c7b56dd7e239bf01f",
+    "url": "/leandro1860/react-calculator/static/css/main.51dd3ffc.chunk.css"
+  },
+  {
+    "revision": "979e858a9f49e1d8f942",
+    "url": "/leandro1860/react-calculator/static/js/2.66696aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leandro1860/react-calculator/static/js/2.66696aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45c7b56dd7e239bf01f",
+    "url": "/leandro1860/react-calculator/static/js/main.d24735be.chunk.js"
+  },
+  {
+    "revision": "158a86783268c4b070b7",
+    "url": "/leandro1860/react-calculator/static/js/runtime-main.4db2c65a.js"
+  }
+]);
